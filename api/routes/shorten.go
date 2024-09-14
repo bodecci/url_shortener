38d743302a076a1c4dd1ada8cbf60b4c38a09e21
Bode <bode.falade@outlook.com>
2024-09-13 21:15:27 -0500

    updated shortener helper func.

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+)
 
 type response struct {
 	URL               string        `json:"url"`
@@ -14,4 +19,38 @@ type request struct {
 	URL         string        `json:"url"`
 	CustomShort string        `json:"short"`
 	Expiry      time.Duration `json:"expiry"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if error := c.BodyParser(&body); error != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	//implement rate limiting
+	// everytime a user queries, check if the IP is already in database,
+	// if yes, decrement the calls remaining by one, else add the IP to database
+	r2 := database
+
+	// check if the input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
+	}
+
+	//check for the domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "haha... nice try",
+		})
+	}
+
+	//enforce http, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
