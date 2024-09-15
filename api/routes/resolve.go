d9fdfcbb68be76aaf38503fbec22c18d1ca9bed2
Bode <bode.falade@outlook.com>
2024-09-14 22:42:56 -0500

    update resolve func to resolve the url.

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"url_shortener/api/database"
 
+	"github.com/go-redis/redis"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,5 +13,24 @@ func ResolveURL(c *fiber.Ctx) error {
 
 	// query the db to find the original URL
 	// if a match is found increment the redirect counter and redirect the original URL
+	// else return error message
 	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found on database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	// redirect to original URL
+	return c.Redirect(value, 301)
 }
