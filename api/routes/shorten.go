c03df7a93d9e89ef924ec068391743369141c8f7
Bode <bode.falade@outlook.com>
2024-09-15 22:41:19 -0500

    add functionality for shorten function.

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"url_shortener/api/database"
+	"url_shortener/api/helpers"
+
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -34,7 +40,24 @@ func ShortenURL(c *fiber.Ctx) error {
 	//implement rate limiting
 	// everytime a user queries, check if the IP is already in database,
 	// if yes, decrement the calls remaining by one, else add the IP to database
-	r2 := database
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	// this user IP has not used our service in the past 30mins
+	// then we store the user IP in our database
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else { // user was found in database
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi((val))
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
