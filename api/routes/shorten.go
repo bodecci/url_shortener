f23cd7af3cd8979f1085df51ff752983fedf93fd
Bode <bode.falade@outlook.com>
2024-09-17 22:41:35 -0500

    finish shorten.go file.

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -15,11 +15,11 @@ import (
 )
 
 type response struct {
-	URL               string        `json:"url"`
-	CustomShortExpiry string        `json:"short`
-	Expiry            time.Duration `json:expiry"`
-	XRateRemaining    int           `json:"rate_limit"`
-	XRateLimitRest    time.Duration `json:"rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short`
+	Expiry          time.Duration `json:expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
 type request struct {
@@ -110,5 +110,21 @@ func ShortenURL(c *fiber.Ctx) error {
 		})
 	}
 
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
 	r2.Decr(database.Ctx, c.IP())
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
